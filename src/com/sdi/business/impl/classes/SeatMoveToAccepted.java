33f7c9e4940656e2931b565423863338e48002a9
Ignacio Fernandez Alvarez <nachoveiga1014@gmail.com>
2016-04-02 22:03:56 +0200

    tiempo de carga de viaje implicado reducido, pero aun ha de reducise mas

--- src/com/sdi/business/impl/classes/SeatMoveToAccepted.java
+++ src/com/sdi/business/impl/classes/SeatMoveToAccepted.java
@@ -1,5 +1,9 @@
 package com.sdi.business.impl.classes;
 
+import java.util.Date;
+
+import alb.util.log.Log;
+
 import com.sdi.infrastructure.Factories;
 import com.sdi.model.Seat;
 import com.sdi.model.SeatStatus;
@@ -9,22 +13,30 @@ import com.sdi.persistence.SeatDao;
 public class SeatMoveToAccepted {
 
 	public void accept(Long idUser, Long idTrip) {
-		SeatDao seatDao = Factories.persistence.newSeatDao();
-		Seat seat = seatDao.findByUserAndTrip(idUser, idTrip);
-		
-		if (seat == null) {
-			seat = new Seat();
-			seat.setUserId(idUser);
-			seat.setTripId(idTrip);
-			seat.setStatus(SeatStatus.ACCEPTED);
-			seatDao.save(seat);
-			decrementAvailablePax(idTrip);
-			Factories.services.createApplicationService().delete(idUser, idTrip);
-		}
-		else if(seat.getStatus().equals(SeatStatus.EXCLUDED)) {
-			seat.setStatus(SeatStatus.ACCEPTED);
-			seatDao.update(seat);
-			decrementAvailablePax(idTrip);
+		Trip trip = Factories.services.createTripService().findTrip(idTrip);
+
+		if (trip.getClosingDate().after(new Date())
+				&& trip.getAvailablePax() > 0) {
+
+			SeatDao seatDao = Factories.persistence.newSeatDao();
+			Seat seat = seatDao.findByUserAndTrip(idUser, idTrip);
+
+			if (seat == null) {
+				seat = new Seat();
+				seat.setUserId(idUser);
+				seat.setTripId(idTrip);
+				seat.setStatus(SeatStatus.ACCEPTED);
+				seatDao.save(seat);
+				decrementAvailablePax(idTrip);
+				Factories.services.createApplicationService().delete(idUser,
+						idTrip);
+				Log.info("Seat Accepted para el usuario [%s] en el viaje [%s] (desde Pending)", idUser, idTrip);
+			} else if (seat.getStatus().equals(SeatStatus.EXCLUDED)) {
+				seat.setStatus(SeatStatus.ACCEPTED);
+				seatDao.update(seat);
+				decrementAvailablePax(idTrip);
+				Log.info("Seat Accepted para el usuario [%s] en el viaje [%s] (desde Excluded)", idUser, idTrip);
+			}
 		}
 	}
 
@@ -32,6 +44,7 @@ public class SeatMoveToAccepted {
 		Trip trip = Factories.services.createTripService().findTrip(idTrip);
 		trip.setAvailablePax(trip.getAvailablePax() - 1);
 		Factories.persistence.newTripDao().update(trip);
+		Log.debug("Plazas disponibles decrementadas en 1 unidad del viaje [%s]", idTrip);
 	}
 
 }
