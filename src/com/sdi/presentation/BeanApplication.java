a9a236abc594ff023239b4eaef841ba7b69a0228
Nauce <naucelg@hotmail.com>
2016-04-04 01:52:49 +0200

    Limpieza pequeña

--- src/com/sdi/presentation/BeanApplication.java
+++ src/com/sdi/presentation/BeanApplication.java
@@ -5,6 +5,11 @@ import javax.faces.bean.ManagedBean;
 
 import com.sdi.business.impl.classes.TripsUpdateStatusTask;
 
+/**
+ * Maneja el mantenimiento programado de la base de datos sobre los estados
+ * de los viajes (OPEN, CLOSED, CANCELLED, DONE)
+ *
+ */
 @ManagedBean (name = "applicationBean", eager = true)
 @ApplicationScoped
 public class BeanApplication {
