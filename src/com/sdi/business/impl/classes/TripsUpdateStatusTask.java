c120d4b1aac0e29d6de7ea00722d1b6abeb7e49f
Nauce <naucelg@hotmail.com>
2016-04-04 02:35:49 +0200

    Fecha estado viaje base de datos fix

--- src/com/sdi/business/impl/classes/TripsUpdateStatusTask.java
+++ src/com/sdi/business/impl/classes/TripsUpdateStatusTask.java
@@ -1,5 +1,6 @@
 package com.sdi.business.impl.classes;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
@@ -20,17 +21,20 @@ public class TripsUpdateStatusTask {
 		@Override
 		public void run() {
 			TripDao dao = Factories.persistence.newTripDao();
-			List<Trip> trips = dao.findAll();
-//			List<Trip> trips = dao.findTravelsOpenAndClosed();
+			List<Trip> trips = dao.findTravelsOpenAndClosed();
+			Date today = DateUtil.today();
 			
 			boolean modified = false;
 			for (Trip trip:trips) {
 				if (!trip.getStatus().equals(TripStatus.CANCELLED)) {
-					if (trip.getArrivalDate().before(DateUtil.today())) {
+					if (trip.getArrivalDate().before(today)
+							|| today.getTime() >= trip.getArrivalDate().getTime()) {
 						trip.setStatus(TripStatus.DONE);
 						modified = true;
 					}
-					else if (trip.getClosingDate().before(DateUtil.today())) {
+					
+					if (trip.getClosingDate().before(today)
+							|| today.getTime() >= trip.getClosingDate().getTime()) {
 						trip.setStatus(TripStatus.CLOSED);
 						modified = true;
 					}
